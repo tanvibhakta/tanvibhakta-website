f01b3bf19878b2bb63bbfdc591a760c3716ffd03
Tanvi Bhakta <tanvibhakta@gmail.com>
2025-04-28 15:16:17 +0530

    Add collections for weeknotes and blogposts

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,27 @@ import { z, defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const poetry = defineCollection({
-  // loader: glob({ pattern: "**/*.md", base: "./src/poetry/" }),
   loader: glob({ pattern: "**/*.md", base: "./posts/poetry/" }),
   schema: z.object({
     title: z.string(),
+    publishedOn: z.string(),
   }),
 });
 
-export const collections = { poetry };
+const weeknotes = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./posts/weeknotes/" }),
+  schema: z.object({
+    title: z.string(),
+    publishedOn: z.string(),
+  }),
+});
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./posts/blog/" }),
+  schema: z.object({
+    title: z.string(),
+    publishedOn: z.string(),
+  }),
+});
+
+export const collections = { poetry, weeknotes, blog };
